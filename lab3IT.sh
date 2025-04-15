6674ad2d5463b3d0784452be748b6a4389bef9ed
maksymmaksym029 <dsw58027@dsw.edu.pl>
2025-04-16 01:16:10 +0200

    Moj pierwszy commit z plikiem .sh

---
+++ lab3IT.sh
@@ -0,0 +1,3 @@
+Dane do przelewu: Uniwersytet Dolnośląski DSW we Wrocławiu, ul. Strzegomska 55, 53-611 Wrocław; nr konta: 
+ING Bank Śląski S.A.  05 1050 1520 1000 0090 3195 9118 
+w tytule przelewu wpisując: nazwisko i imię uczestnika, kurs z matematyki
